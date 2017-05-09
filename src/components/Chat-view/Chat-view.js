6337159e2d498933638e17815bec038ef8b4839b
Alexander Mortensen <alextmortensen@gmail.com>
2017-05-08 23:03:37 -0600

    adds socketio connection

--- src/components/Chat-view/Chat-view.js
+++ src/components/Chat-view/Chat-view.js
@@ -2,8 +2,23 @@ import React, {Component} from 'react'
 import {Text, StyleSheet} from 'react-native'
 import {connect} from 'react-redux'
 import Nav from '../Nav'
+import io from 'socket.io-client'
+
+console.ignoredYellowBox = ['Setting a timer for a long period of time']
 
 class Chat extends Component{
+	constructor(){
+		super()
+
+		this.socket = io('http://192.168.1.4:3000', {
+		  transports: ['websocket']
+		})
+	}
+
+	componentWillUnmount(){
+		this.socket.disconnect()
+	}
+
 	render(){
 		return(
 			<Nav>
